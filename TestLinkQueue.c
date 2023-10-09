df4a2332be05f91ea8ab987bbffa1ae14e8bdf10
knu-kimdohoon <105111237+knu-kimdohoon@users.noreply.github.com>
2023-10-09 16:20:57 +0900

    Update TestLinkQueue.c

--- TestLinkQueue.c
+++ TestLinkQueue.c
@@ -1,7 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include "Queue_Link.h"
+
 
 void main() {
 	Queue* pQueue = CreateQueue();
@@ -9,15 +12,25 @@ void main() {
 	//printf("pQueue->count : %d\n", pQueue->count);
 	char name[20];
 	char* data;
+	char* nPtr = NULL;
 	int i;
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 4; i++) {
 		printf("Name : ");
 		scanf("%[^\n]s", name);
-		Enqueue(pQueue, name);
-		scanf("%*c");
-		data = TopQueue(pQueue);
-		printf("Top data : %s\n", data);
-		PrintAllDataInTheQueue(pQueue);
+		nPtr = (char*)malloc((strlen(name)+2) * sizeof(char));
+		if (nPtr == NULL) {
+			exit(1);
+		}
+		else {
+			strcpy(nPtr, name);
+			printf("%s\n", nPtr);
+			Enqueue(pQueue, nPtr);
+			free(nPtr);
+			scanf("%*c");
+			data = TopQueue(pQueue);
+			printf("Top data : %s\n", data);
+			PrintAllDataInTheQueue(pQueue);
+		}
 	}
 
 	
